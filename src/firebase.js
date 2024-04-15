779926200717e015aeed64c9a01af69f1b20910a
AmirRosli <4mirosli@gmail.com>
2024-04-15 17:47:13 +0800

    add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// x xzhjvhjvjjhhjuyfyuffuydtyttyvsvds
+// x xzhjvhjvjjhhjuyfyuffuydtyttyvsvdsdscs
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
